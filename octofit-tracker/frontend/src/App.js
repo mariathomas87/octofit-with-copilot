f50d299659dd27be9e3ada2f9fa809fa6c40ed55
Maria Thomas <maria_marina_04@yahoo.co.in>
2025-04-08 23:16:22 +0000

    Moved frontend folder and updated .gitignore

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -6,19 +6,14 @@ import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
 import './App.css';
-import logo from './octofitapp-small.png';
 
 function App() {
   return (
     <Router>
       <div className="container">
-        <header className="logo">
-          <img src={logo} alt="OctoFit Logo" />
-          <h1>OctoFit Tracker</h1>
-        </header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">Home</Link>
+            <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
